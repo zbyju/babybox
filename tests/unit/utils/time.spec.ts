f1f4a6b4b0f7694a4c6df7fde476e41dfbf9677d
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-06-24 01:08:35 +0200

    Create test suite for time utils

--- tests/unit/utils/time.spec.ts
+++ tests/unit/utils/time.spec.ts
@@ -1,5 +1,79 @@
-import { getCurrentTimePC } from "@/utils/time";
+import {
+  getCurrentTimePC,
+  getHoursWithLeadingZeroes,
+  getMinutesWithLeadingZeroes,
+} from "@/utils/time";
+import moment from "moment";
 
-test("Current time PC is now", () => {
-  expect(getCurrentTimePC().diff(Date.now())).toBeCloseTo(0);
+describe("getCurrentTimePC function", () => {
+  test("Should return time that is now", () => {
+    expect(getCurrentTimePC().diff(Date.now())).toBeCloseTo(0);
+  });
+});
+
+describe("getHoursWithLeadingZeroes function", () => {
+  test("Should return time as is when valid and 2 digits", () => {
+    expect(getHoursWithLeadingZeroes(moment().hours(12))).toBe("12");
+    expect(
+      getHoursWithLeadingZeroes(moment().hours(12).minutes(23).seconds(39))
+    ).toBe("12");
+    expect(
+      getHoursWithLeadingZeroes(
+        moment().hours(23).minutes(59).seconds(59).milliseconds(999)
+      )
+    ).toBe("23");
+  });
+
+  test("Should add leading zeroes when missing", () => {
+    expect(getHoursWithLeadingZeroes(moment().hours(2))).toBe("02");
+    expect(getHoursWithLeadingZeroes(moment().hours(0))).toBe("00");
+    expect(getHoursWithLeadingZeroes(moment().hours(24))).toBe("00");
+    expect(
+      getHoursWithLeadingZeroes(
+        moment().hours(9).minutes(59).seconds(59).milliseconds(999)
+      )
+    ).toBe("09");
+    expect(
+      getHoursWithLeadingZeroes(moment().hours(2).minutes(23).seconds(39))
+    ).toBe("02");
+  });
+
+  test("Should return -- when time is not valid", () => {
+    expect(getHoursWithLeadingZeroes(null)).toBe("--");
+    expect(getHoursWithLeadingZeroes(undefined)).toBe("--");
+  });
+});
+
+describe("getMinutesWithLeadingZeroes function", () => {
+  test("Should return time as is when valid and 2 digits", () => {
+    expect(getMinutesWithLeadingZeroes(moment().minutes(12))).toBe("12");
+    expect(
+      getMinutesWithLeadingZeroes(moment().hours(12).minutes(23).seconds(39))
+    ).toBe("23");
+    expect(
+      getMinutesWithLeadingZeroes(
+        moment().hours(23).minutes(59).seconds(59).milliseconds(999)
+      )
+    ).toBe("59");
+  });
+
+  test("Should add leading zeroes when missing", () => {
+    expect(getMinutesWithLeadingZeroes(moment().minutes(2))).toBe("02");
+    expect(getMinutesWithLeadingZeroes(moment().minutes(0))).toBe("00");
+    expect(getMinutesWithLeadingZeroes(moment().minutes(60))).toBe("00");
+    expect(
+      getHoursWithLeadingZeroes(
+        moment().hours(9).minutes(9).seconds(59).milliseconds(999)
+      )
+    ).toBe("09");
+    expect(
+      getHoursWithLeadingZeroes(moment().hours(2).minutes(2).seconds(39))
+    ).toBe("02");
+  });
+
+  test("Should return -- when time is not valid", () => {
+    expect(getHoursWithLeadingZeroes(null)).toBe("--");
+    expect(getHoursWithLeadingZeroes(undefined)).toBe("--");
+    expect(getHoursWithLeadingZeroes(moment([2015, 25, 35]))).toBe("--");
+  });
 });
