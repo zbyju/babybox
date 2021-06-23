f50997528f978f0e50ed595ba9f085b38260e746
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-06-23 16:09:07 +0200

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+
   </div>
-  <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    //Main
+  }
+});
+</script>
+
 
 <style lang="stylus">
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
+  font-family "Open Sans", Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+.time-font
+  font-family Impact, Avenir, Helvetica, Arial, sans-serif
+  font-weight 800
+
+html, body
+  padding 0
+  margin 0
+
+.w-300
+  font-weight 300
+
+.w-400
+  font-weight 400
+
+.w-600
+  font-weight 600
+
+.w-700
+  font-weight 700
+
+.w-800
+  font-weight 800
+
+.fs-italic
+  font-style italic
+
+.fs-normal
+  font-style normal
+
 </style>
