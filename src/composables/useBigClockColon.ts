23fe106083b5a0b8260443866924444af61c81d0
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-06-24 20:25:20 +0200

    Fix colon blinking too fast

--- src/composables/useBigClockColon.ts
+++ src/composables/useBigClockColon.ts
@@ -1,9 +1,14 @@
-import { ref, onMounted, watch } from "vue";
+import moment from "moment";
+import { ref, watch, ComputedRef } from "vue";
 
-export default function useUserRepositories(time) {
+export default function useBigClockColon(time: ComputedRef<moment.Moment>) {
   const showColon = ref(true);
+  let prevTime = time.value;
   const toggleColon = () => {
-    showColon.value = !showColon.value;
+    if (moment(time.value).diff(prevTime, "seconds") > 0.4) {
+      showColon.value = !showColon.value;
+      prevTime = time.value;
+    }
   };
   watch(time, toggleColon);
 
