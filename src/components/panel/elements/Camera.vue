e129e8c56fd2ec37bc88965219054d44d9569bb8
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-06-24 20:24:06 +0200

    Add camera component

---
+++ src/components/panel/elements/Camera.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="CameraWrapper">
+    <img :src="url" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import useCamera from "@/composables/useCamera";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const config = store.state.config.camera;
+    const url = useCamera(config);
+
+    return { url };
+  },
+});
+</script>
+
+<style lang="stylus">
+#CameraWrapper
+  grid-area camera
+  max-width 100%
+  overflow hidden
+
+  display flex
+  flex-direction row
+  justify-content center
+  align-items center
+
+  background-color primary
+
+  img
+    border-width = 3px
+    width "calc(100% - (%s * 2))" % border-width
+    margin auto
+    border border-width solid secondary
+    padding 5px
+</style>
---
+++ src/composables/useCamera.ts
@@ -0,0 +1,14 @@
+import { computed, watch, ref, onMounted } from "vue";
+import { CameraConfig } from "@/types/main";
+
+export default function useCamera(config: CameraConfig) {
+  const time = ref(new Date().getTime().toString());
+  const url = ref("");
+  onMounted(() => {
+    setInterval(() => {
+      time.value = new Date().getTime().toString();
+      url.value = `http://${config.username}:${config.password}@${config.ip}/cgi-bin/snapshot.cgi?Channel=0/${time.value}`;
+    }, config.updateDelay);
+  });
+  return url;
+}
