e104715807b8abacb799cf264207129305a03fdd
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-07-02 02:16:07 +0200

    Test composable function useCamera

--- src/composables/useCamera.ts
+++ src/composables/useCamera.ts
@@ -1,14 +1,14 @@
-import { ref, onMounted } from "vue";
+import { ref, Ref } from "vue";
 import { CameraConfig } from "@/types/main";
 
-export default function useCamera(config: CameraConfig) {
-  const time = ref(new Date().getTime().toString());
+export default function useCamera(config: CameraConfig): Ref<string> {
   const url = ref("");
-  onMounted(() => {
-    setInterval(() => {
-      time.value = new Date().getTime().toString();
-      url.value = `http://${config.username}:${config.password}@${config.ip}/cgi-bin/snapshot.cgi?Channel=0/${time.value}`;
-    }, config.updateDelay);
-  });
+
+  // Update camera URL (timestamp) every @config.updateDelay miliseconds - resulting in updating the image
+  setInterval(() => {
+    const time = new Date().getTime().toString();
+    url.value = `http://${config.username}:${config.password}@${config.ip}/cgi-bin/snapshot.cgi?Channel=0/${time}`;
+  }, config.updateDelay);
+
   return url;
 }
---
+++ tests/unit/composable/useCamera.spec.ts
@@ -0,0 +1,65 @@
+import useCamera from "@/composables/useCamera";
+import { CameraConfig } from "@/types/main";
+
+const cfg: CameraConfig = {
+  ip: "ip",
+  username: "user",
+  password: "pass",
+  updateDelay: 10,
+};
+
+describe("useCamera composable function", () => {
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test("URL should be initially defined", () => {
+    const result = useCamera(cfg);
+    expect(result.value).toBeDefined();
+  });
+
+  test("URL should contain config credentials and ip", () => {
+    jest.useFakeTimers();
+    const result = useCamera(cfg);
+    jest.advanceTimersByTime(cfg.updateDelay * 1.5);
+    expect(setInterval).toHaveBeenCalledTimes(1);
+    expect(setInterval).toHaveBeenLastCalledWith(
+      expect.any(Function),
+      cfg.updateDelay
+    );
+    expect(result.value.length).toBeGreaterThan(
+      cfg.ip.length + cfg.username.length + cfg.password.length
+    );
+    expect(result.value).toContain(cfg.ip);
+    expect(result.value).toContain(cfg.username);
+    expect(result.value).toContain(cfg.password);
+  });
+
+  test("URL end with a timestamp that is now", () => {
+    jest.useFakeTimers();
+    const result = useCamera(cfg);
+    jest.advanceTimersByTime(cfg.updateDelay * 1.5);
+    // Get url and remove last / if it is there
+    const url =
+      result.value[result.value.length - 1] === "/"
+        ? result.value.slice(0, -1)
+        : result.value;
+    const time: number = new Date().getTime();
+    // Get the timestamp from url
+    const timeUrl: number = parseInt(
+      url.substr(url.length - time.toString().length)
+    );
+    expect(Math.abs(time - timeUrl)).toBeLessThan(cfg.updateDelay * 100);
+  });
+
+  test("URL should change with time", () => {
+    jest.useFakeTimers();
+    const result = useCamera(cfg);
+    const NUMBER_OF_TESTS = 5;
+    for (let i = 0; i < NUMBER_OF_TESTS; ++i) {
+      const url = result.value;
+      jest.advanceTimersByTime(10000);
+      expect(result.value).not.toEqual(url);
+    }
+  });
+});
