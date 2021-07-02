ee7d1aab6e5166c98c271860667d278d5d0b106e
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-07-02 02:15:48 +0200

    Add constants and comments

--- src/composables/useBigClockColon.ts
+++ src/composables/useBigClockColon.ts
@@ -7,14 +7,18 @@ export default function useBigClockColon(time: ComputedRef<moment.Moment>) {
 
   const showColon = ref(true);
 
+  // Colon blinks for @BLINK_DELAY miliseconds
   const blink = () => {
     showColon.value = !showColon.value;
     setTimeout(() => {
       showColon.value = !showColon.value;
     }, BLINK_DELAY);
   };
+
+  // Blink at max once every @BLINK_DELAY * 2 miliseconds
   const throttledBlink = _.throttle(blink, BLINK_DELAY * 2);
 
+  // Blink when time changes
   watch(time, throttledBlink);
 
   return {
--- src/composables/useDelayedMessage.ts
+++ src/composables/useDelayedMessage.ts
@@ -2,18 +2,23 @@ import { computed, watch, ref, ComputedRef } from "vue";
 import { Message } from "@/types/main";
 
 export default function useCamera(message: ComputedRef<Message>) {
+  const MESSAGE_DELAY = 1000;
+
   const messageDelayed = ref(message.value);
   const heightStyle = computed(() => {
     return message.value ? "visible" : "hidden";
   });
+
+  // If message is getting set - update immediately, if it is getitng removed - update after @MESSAGE_DELAY miliseconds
   watch(message, () => {
     if (message.value) {
       messageDelayed.value = message.value;
     } else {
       setTimeout(() => {
         messageDelayed.value = message.value;
-      }, 1000);
+      }, MESSAGE_DELAY);
     }
   });
+
   return { messageDelayed, heightStyle };
 }
