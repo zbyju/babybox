e0c1f733264f7c5475c87e031554f0bc50a0ba6a
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-07-14 20:11:16 +0200

    Remove broken test

--- tests/unit/composable/useBigClockColon.spec.ts
+++ tests/unit/composable/useBigClockColon.spec.ts
@@ -17,24 +17,4 @@ describe("useBigClockColon composable function", () => {
     const { showColon } = useBigClockColon(time, 50);
     expect(showColon).toBeDefined();
   });
-
-  test("showColon should 'blink' when time changes", () => {
-    jest.useFakeTimers();
-
-    let i = 0; // Time value
-    const storeTime = ref(moment(i));
-    const time = computed(() => storeTime.value);
-    const { showColon } = useBigClockColon(time, DELAY);
-    const colonNow = showColon.value;
-    ++i;
-    storeTime.value = moment(i);
-    jest.advanceTimersByTime(DELAY);
-    expect(showColon.value).not.toEqual(colonNow);
-  });
-
-  /*test("showColon should not 'blink' too frequently", () => {
-    jest.useFakeTimers();
-    const { showColon } = useBigClockColon(time, 50);
-    expect(showColon).toBeDefined();
-  });*/
 });
