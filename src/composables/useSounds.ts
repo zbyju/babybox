a74fab7a82a3393ac5fd0a561225c1fd181c1c88
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-06 17:29:54 +0200

    Add SoundPlayer to control what sound is played

--- src/composables/useSounds.ts
+++ src/composables/useSounds.ts
@@ -1,65 +1,37 @@
-interface Sound {
-  name: string;
-  sound: any;
-}
-
-export interface BabyboxSounds {
-  activation: Sound;
-  wasOpened: Sound;
-  opening: Sound;
-  start: Sound;
-  connectionLost: Sound;
-}
-
 import { Howl } from "howler";
 
-export const useSounds = (): BabyboxSounds => {
-  const activation = require("@/assets/sounds/Aktivace.mp3");
-  const wasOpened = require("@/assets/sounds/BylOtevren.mp3");
-  const opening = require("@/assets/sounds/Otevirani.mp3");
-  const start = require("@/assets/sounds/Start.mp3");
-  const connectionLost = require("@/assets/sounds/ZtrataSpojeni.mp3");
-
-  return {
-    activation: {
-      name: "Aktivace",
-      sound: new Howl({
-        src: [activation],
-        autoplay: false,
-        loop: true,
-      }),
-    },
-    wasOpened: {
-      name: "BylOtevren",
-      sound: new Howl({
-        src: [wasOpened],
-        autoplay: false,
-        loop: true,
-      }),
-    },
-    opening: {
-      name: "Otevirani",
-      sound: new Howl({
-        src: [opening],
-        autoplay: false,
-        loop: true,
-      }),
-    },
-    start: {
-      name: "Start",
-      sound: new Howl({
-        src: [start],
-        autoplay: false,
-        loop: true,
-      }),
-    },
-    connectionLost: {
-      name: "ZtrataSpojeni",
-      sound: new Howl({
-        src: [connectionLost],
-        autoplay: false,
-        loop: true,
-      }),
-    },
-  };
+export const useSounds = () => {
+  return new BabyboxSoundPlayer();
 };
+
+class BabyboxSoundPlayer {
+  playing;
+  private readonly SOUND_PATH: string;
+
+  constructor() {
+    this.playing = null;
+    this.SOUND_PATH = "@/assets/sounds/";
+  }
+
+  private loadHowl(name: string, loop: boolean) {
+    const sound = require(`@/assets/sounds/${name}.mp3`);
+    return new Howl({
+      src: [sound],
+      autoplay: false,
+      loop: loop,
+    });
+  }
+
+  playSound(name: string, loop = true) {
+    if (this.playing) this.playing.stop();
+
+    const howl = this.loadHowl(name, loop);
+    this.playing = howl;
+
+    this.playing.play();
+  }
+
+  stopSound() {
+    this.playing.stop();
+  }
+}
