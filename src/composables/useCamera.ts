a2c47ae48d79cdbef9840f880d41a874125d10ee
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-09-06 15:13:59 +0200

    Change image url to use the video instead of image

--- src/composables/useCamera.ts
+++ src/composables/useCamera.ts
@@ -4,14 +4,17 @@ import { getURLPostfix, stringToCameraType } from "@/utils/panel/camera";
 
 export default function useCamera(config: CameraConfig): Ref<string> {
   const url = ref("");
-
-  // Update camera URL (timestamp) every @config.updateDelay miliseconds - resulting in updating the image
-  setInterval(() => {
-    const time = new Date().getTime().toString();
-    url.value = `http://${config.username}:${config.password}@${
-      config.ip
-    }${getURLPostfix(stringToCameraType(config.cameraType))}${time}`;
-  }, config.updateDelay);
+  if (config.cameraType === "dahua") {
+    url.value = `http://${config.username}:${config.password}@${config.ip}/cgi-bin/mjpg/video.cgi?channel=1&subtype=1`;
+  } else {
+    // Update camera URL (timestamp) every @config.updateDelay miliseconds - resulting in updating the image
+    setInterval(() => {
+      const time = new Date().getTime().toString();
+      url.value = `http://${config.username}:${config.password}@${
+        config.ip
+      }${getURLPostfix(stringToCameraType(config.cameraType))}${time}`;
+    }, config.updateDelay);
+  }
 
   return url;
 }
