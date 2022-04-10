eecec6b8d1339fb4c632c18c937814a97c614e75
zbyju <zbynek.jurica@gmail.com>
2022-04-10 16:29:36 +0200

    Add state to appstate store

--- src/pinia/appStateStore.ts
+++ src/pinia/appStateStore.ts
@@ -1,6 +1,17 @@
 import { defineStore } from "pinia";
 
+export interface AppStateMessage {
+  text: string;
+  color: string;
+  sound?: string;
+}
+
+export type AppStateActive = boolean;
+
 export const useStore = defineStore("main", {
-  state: () => ({}),
+  state: () => ({
+    message: null as AppStateMessage,
+    active: false as AppStateActive,
+  }),
   actions: {},
 });
