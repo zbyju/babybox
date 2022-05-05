5fdfe417fac9db48366d69c67386fd4eb6b8663b
zbyju <zbynek.jurica@gmail.com>
2022-05-05 16:43:10 +0200

    Remove # from url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 import Data from "../views/Data.vue";
 import Main from "../views/Main.vue";
@@ -23,7 +23,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
