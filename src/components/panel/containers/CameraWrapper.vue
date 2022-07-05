36504c5eacab3d7f434e7e99ba830289f0b3f411
zbyju <zbynek.jurica@gmail.com>
2022-07-05 19:01:58 +0200

    Fix linting a formatting configuration

--- src/components/panel/containers/CameraWrapper.vue
+++ src/components/panel/containers/CameraWrapper.vue
@@ -1,55 +1,58 @@
 <template>
-    <div
-        id="CameraWrapper"
-        :style="{ maxHeight: props.maxH, maxWidth: props.maxW }">
-        <div id="DoorBars" v-if="displayDoors">
-            <HorizontalPositionBar
-                :maxValue="maxDoors"
-                :minValue="minDoors"
-                :value="leftDoors"
-                :direction="'row'" />
-            <HorizontalPositionBar
-                :maxValue="maxDoors"
-                :minValue="minDoors"
-                :value="rightDoors"
-                :direction="'row-reverse'" />
-        </div>
-        <CameraView :displayTopBorder="displayDoors === false" />
+  <div
+    id="CameraWrapper"
+    :style="{ maxHeight: props.maxH, maxWidth: props.maxW }"
+  >
+    <div id="DoorBars" v-if="displayDoors">
+      <HorizontalPositionBar
+        :maxValue="maxDoors"
+        :minValue="minDoors"
+        :value="leftDoors"
+        :direction="'row'"
+      />
+      <HorizontalPositionBar
+        :maxValue="maxDoors"
+        :minValue="minDoors"
+        :value="rightDoors"
+        :direction="'row-reverse'"
+      />
     </div>
+    <CameraView :displayTopBorder="displayDoors === false" />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import CameraView from "@/components/panel/elements/CameraView.vue";
-import HorizontalPositionBar from "@/components/panel/elements/HorizontalPositionBar.vue";
-import { useUnitsStore } from "@/pinia/unitsStore";
-import { storeToRefs } from "pinia";
-import { computed } from "vue";
+  import CameraView from "@/components/panel/elements/CameraView.vue";
+  import HorizontalPositionBar from "@/components/panel/elements/HorizontalPositionBar.vue";
+  import { useUnitsStore } from "@/pinia/unitsStore";
+  import { storeToRefs } from "pinia";
+  import { computed } from "vue";
 
-const props = defineProps({
+  const props = defineProps({
     maxW: String,
     maxH: String,
     displayDoors: Boolean,
-});
+  });
 
-const unitsStore = useUnitsStore();
-const { engineUnit } = storeToRefs(unitsStore);
-const minDoors = computed(() => parseInt(engineUnit.value[2].value));
-const maxDoors = computed(() => parseInt(engineUnit.value[3].value));
-const leftDoors = computed(() => parseInt(engineUnit.value[37].value));
-const rightDoors = computed(() => parseInt(engineUnit.value[38].value));
+  const unitsStore = useUnitsStore();
+  const { engineUnit } = storeToRefs(unitsStore);
+  const minDoors = computed(() => parseInt(engineUnit.value[2].value));
+  const maxDoors = computed(() => parseInt(engineUnit.value[3].value));
+  const leftDoors = computed(() => parseInt(engineUnit.value[37].value));
+  const rightDoors = computed(() => parseInt(engineUnit.value[38].value));
 </script>
 
 <style lang="stylus">
-#CameraWrapper
-  aspect-ratio: 16/9
-  flex-grow 1
-  max-height 100%
-
-  #DoorBars
-    width 100%
+  #CameraWrapper
+    aspect-ratio: 16/9
+    flex-grow 1
     max-height 100%
-    display flex
-    flex-direction row
-    justify-content space-between
-    gap 5px
+
+    #DoorBars
+      width 100%
+      max-height 100%
+      display flex
+      flex-direction row
+      justify-content space-between
+      gap 5px
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,22 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 4000,
+  server: {
+    port: 4000,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
+  },
+  css: {
+    preprocessorOptions: {
+      stylus: {
+        imports: [
+          path.resolve(__dirname, "./src/assets/styles/variables.styl"),
+        ],
+      },
     },
-    css: {
-        preprocessorOptions: {
-            stylus: {
-                imports: [
-                    path.resolve(
-                        __dirname,
-                        "./src/assets/styles/variables.styl"
-                    ),
-                ],
-            },
-        },
-    },
+  },
 });
