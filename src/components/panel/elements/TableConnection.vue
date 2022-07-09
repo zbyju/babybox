bf54d01e9fe4817cf0628567d8b3d5c4e3929859
zbyju <zbynek.jurica@gmail.com>
2022-07-09 13:17:46 +0200

    Store changes

--- src/components/panel/elements/TableConnection.vue
+++ src/components/panel/elements/TableConnection.vue
@@ -8,6 +8,11 @@
   import BaseTable from "@/components/panel/HTMLElements/BaseTable.vue";
   import { useConnectionStore } from "@/pinia/connectionStore";
   import { useUnitsStore } from "@/pinia/unitsStore";
+  import type {
+    RowData,
+    TableConnectionValues,
+    TableData,
+  } from "@/types/panel/tables.types";
   import { getRowsTableConnection } from "@/utils/panel/tables";
   import { storeToRefs } from "pinia";
   import { computed } from "vue";
@@ -17,12 +22,14 @@
   const { engineUnit, thermalUnit } = storeToRefs(unitsStore);
   // TODO: Check to refactor
   const { engineUnit: eu, thermalUnit: tu } = storeToRefs(connectionStore);
-  const rows = computed(() => {
-    return getRowsTableConnection(engineUnit.value, thermalUnit.value, {
-      engineUnit: eu.value,
-      thermalUnit: tu.value,
-    });
-  });
+  const values: TableConnectionValues = undefined;
+  const rows: TableData = computed(() => [{
+    label: ,
+    value: ,
+    error: ,
+    success: ,
+    warning: ,
+  }]);
   const title = "Spojení PC ↔ BB";
 </script>
 
--- src/types/panel/tables.types.ts
+++ src/types/panel/tables.types.ts
@@ -1,6 +1,10 @@
+import type { Maybe } from "../generic.types";
+
 export interface RowData {
   label: string;
   value: string;
+  loading?: boolean;
+  failedFetching?: boolean;
   error?: boolean;
   success?: boolean;
   warning?: boolean;
@@ -18,3 +22,111 @@ export type BlockRows = BlockData[];
 
 export type TableData = RowData[];
 export type TableBlockData = BlockRows[];
+
+export interface TableTemperaturesValues {
+  blockData: {
+    isHeatingCasing: {
+      value: boolean;
+    };
+    isHeatingAir: {
+      value: boolean;
+    };
+    isCoolingAir: {
+      value: boolean;
+    };
+  };
+  rowData: {
+    optimalTemperature: {
+      value: string;
+    };
+    innerTemperature: {
+      value: string;
+    };
+    outsideTemperature: {
+      value: string;
+    };
+    bottomTemperature: {
+      value: string;
+    };
+    topTemperature: {
+      value: string;
+    };
+    casingTemperature: {
+      value: string;
+    };
+  };
+}
+
+export interface TableDoorsValues {
+  blockData: {
+    isBlocked: {
+      value: boolean;
+    };
+  };
+  rowData: {
+    leftPosition: {
+      value: string;
+    };
+    rightPosition: {
+      value: string;
+    };
+    leftLoad: {
+      value: string;
+    };
+    rightLoad: {
+      value: string;
+    };
+    beamAboveContainer: {
+      value: string;
+    };
+  };
+}
+
+export interface TableVoltagesValues {
+  rowData: {
+    inVoltage: {
+      value: string;
+    };
+    batteryVoltage: {
+      value: string;
+    };
+    unitsVoltage: {
+      value: string;
+    };
+    gsmVoltage: {
+      value: string;
+    };
+    inGoalVoltage: {
+      value: string;
+    };
+    batteryGoalVoltage: {
+      value: string;
+    };
+    unitsGoalVoltage: {
+      value: string;
+    };
+  };
+}
+
+export interface TableConnectionValues {
+  rowData: {
+    requests: {
+      value: string;
+    };
+    successfulRequests: {
+      value: string;
+    };
+    failedRequests: {
+      value: string;
+    };
+    quality: {
+      value: string;
+    };
+    timeout: {
+      value: string;
+    };
+    timeToInspection: {
+      value: string;
+    };
+  };
+}
