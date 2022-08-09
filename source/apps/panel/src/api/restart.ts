1bb1ce2fe818230a5070f5b5024d15fdb251b678
zbyju <zbynek.jurica@gmail.com>
2022-08-09 14:40:34 +0200

    Fix restart not having timeout

---
+++ source/apps/panel/src/api/restart.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { storeToRefs } from "pinia";
+
+import { useConfigStore } from "@/pinia/configStore";
+
+export const refreshRestartCooldown = () => {
+  const configStore = useConfigStore();
+  const { api } = storeToRefs(configStore);
+  const url = api.value.baseApiUrl + "/restart/refresh";
+  const timeout = api.value.requestTimeout || 5000;
+
+  return axios.get(url, { timeout: timeout });
+};
