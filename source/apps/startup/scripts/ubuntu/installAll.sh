2a2a81058f08a8fbd0a8e0196ec2c6907263b748
zbyju <zbynek.jurica@gmail.com>
2023-08-30 23:43:05 +0200

    Fix install script

--- source/apps/startup/scripts/ubuntu/installAll.sh
+++ source/apps/startup/scripts/ubuntu/installAll.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd ~
+sudo apt update
+sudo apt upgrade
+sudo apt update
 
 # ----- Install teamviewer -----
 
@@ -8,15 +11,18 @@ echo "          ----- INSTALLING TEAMVIEWER -----"
 # Download teamviewer
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 # Install teamviewer 
-sudo apt install -y ./teamviewer_amd64.deb
+sudo apt install -q -y ./teamviewer_amd64.deb
 # Disable WayLand (otherwise you can't connect to the remote computer without a prompt to the user)
-sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false' /etc/gdm3/custom.conf
+sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
 # Remove the installation file
 rm -rf ./teamviewer_amd64.deb
 
 # ----- Install npm -----
 echo "          ----- INSTALLING NPM -----"
-sudo apt install npm
+sudo apt install -q -y npm
+source ~/.profile
+source ~/.bashrc
+source /etc/profile
 
 # Allow global packages to be installed and ran without sudo priviliges
 mkdir ~/.npm-global
@@ -26,6 +32,7 @@ echo "export PATH=$PATH:$(npm get prefix)/bin" >> ~/.bashrc
 echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
 source ~/.profile
 source ~/.bashrc
+source /etc/profile
 
 # ----- Install n (dependency: npm) -----
 echo "          ----- INSTALLING N -----"
@@ -34,10 +41,16 @@ sudo mkdir -p /usr/local/n
 sudo chown -R $(whoami) /usr/local/n
 sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
 sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+source ~/.profile
+source ~/.bashrc
+source /etc/profile
 
 # ----- Install Node.js (dependency: n, npm)
 echo "          ----- INSTALLING NODE.JS -----"
 n lts
+source ~/.profile
+source ~/.bashrc
+source /etc/profile
 
 # ----- Remove updater ----- 
 echo "          ----- REMOVING UPDATE MANAGER -----"
@@ -48,15 +61,23 @@ cd ~
 git config --global credential.helper store
 git clone https://github.com/zbyju/babybox.git
 cd ~/babybox/source/apps/startup/scripts/ubuntu
-./install.sh
-sudo apt-get install -y fonts-open-sans
+source ~/.profile
+source ~/.bashrc
+source /etc/profile
 
 # ----- Enable starting panel on PC startup -----
+chmod +x /home/babybox/source/apps/startup/scripts/ubuntu/startup.sh
+mkdir -p ~/.config/autostart
 touch ~/.config/autostart/babybox.desktop
 echo "[Desktop Entry]
 Type=Application
 Name=Babybox
-Exec=~/babybox/source/apps/startup/scripts/ubuntu/startup.sh
+Exec=/home/babybox/source/apps/startup/scripts/ubuntu/startup.sh
 Comment=BabyboxPanel
-X-GNOME-Autostart-enabled=true" >> ~/.config/autostart/babybox.desktop
+X-GNOME-Autostart-enabled=true" > ~/.config/autostart/babybox.desktop
+
+sudo apt-get install -y fonts-open-sans
+
+./install.sh
+
 
